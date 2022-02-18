729382030bb42a7bae2817a00f9b507e46fbd1c0
Krishani Desai <krishani.d@somaiya.edu>
2022-02-18 22:26:38 +0530

    game done

---
+++ challenge/src/icons/Paper.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      x="0px"
+      y="0px"
+      viewBox="0 0 100 100"
+    >
+      <text x="20" y="60" fontSize="30">Fire</text>
+    </svg>
+  );
+}
